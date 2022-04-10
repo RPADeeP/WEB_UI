6ca4e62a36b8f89eb561f735c5c7df380f2b1d98
RustamBaratov22 <77454010+YooGun@users.noreply.github.com>
2022-04-10 16:21:13 +0600

    логин

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginService } from './login.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  constructor(private http: HttpClient, private loginService: LoginService) { }
+  password: User;
+  code: User;
+
+  onClickLogin(){
+    this.loginService.login(this.code,this.password).subscribe(
+    );
+
+  }
+}
---
+++ src/app/views/login/user.ts
@@ -0,0 +1,4 @@
+export class User {
+    code: string;
+    password: string;
+    }
